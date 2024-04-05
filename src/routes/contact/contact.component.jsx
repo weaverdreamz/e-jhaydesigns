c9d71b8fa0d62c81ff32bd5ca018eeaab9f4a18e
weaver <jojouna419@gmail.com>
2024-04-05 14:58:15 +0100

    Addes the contact page

--- src/routes/contact/contact.component.jsx
+++ src/routes/contact/contact.component.jsx
@@ -2,18 +2,47 @@ import './contact.scss';
 import {useState} from 'react';
 import Input from '../../components/input/input.component';
 import Button from '../../components/button/button.component';
+import emailjs from '@emailjs/browser';
 
-const defaultVal = {name:'', email:''};
+
+const defaultVal = {name:'', email:'', textspace:''};
 
 const Contact = ()=>{
-
+    
     const [contact, setContact] = useState(defaultVal);
-    const {name, email} = contact;
+    const {name, email, textspace} = contact;
 
     const inputHandler = (event)=>{
         const {name, value} = event.target;
         setContact({...contact, [name]:value});
     }
+
+    console.log(textspace);
+
+    const sendMessage = ()=>{
+
+        const templateParams = {
+          to_name:"MADEBYJHAY",
+          from_name: name,
+          message: `Customer Name: ${name}, Customer Email: ${email}, Message: ${textspace}`
+        };
+        
+        emailjs
+          .send('service_090999', 'template_92osu0q', templateParams, {
+            publicKey: 'vKMXZcDAiKaTBNDOd',
+          })
+          .then(
+            (response) => {
+              alert('SUCCESS!', response.status, response.text);
+              setContact(defaultVal);
+            },
+            (err) => {
+              console.log('FAILED...', err);
+            },
+          );
+         
+      }
+  
     
     return(
         <div className='contact'>
@@ -29,8 +58,8 @@ const Contact = ()=>{
                 <Input name='name' value={name} type='text' required onChange={inputHandler}/>
                 <h5 className='second'>Email</h5>
                 <Input name='email' value={email} type='email' required onChange={inputHandler}/>
-                <textarea name='complaint' placeholder="How may we help you?"/>
-                <Button type='button' children='CONTACT' className='signed'/>
+                <textarea name='textspace' placeholder="How may we help you?" value={textspace} onChange={inputHandler} />
+                <Button type='button' children='CONTACT' className='signed' onClick={sendMessage}/>
 
             </form>
         </div>
